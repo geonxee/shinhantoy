49fa7ae2c91dbd7d200156886575996f1cfd8243
geonxee <1913kg@naver.com>
2023-01-20 12:38:04 +0900

    init

--- shinhantoy/shinhantoy/asgi.py
+++ shinhantoy/shinhantoy/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for geonhee project.
+ASGI config for shinhantoy project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geonhee.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shinhantoy.settings')
 
 application = get_asgi_application()
