7a0a4c1beedc375a4c8e5cd2c69902c2f433ec7d
geonxee <1913kg@naver.com>
2023-01-26 17:40:26 +0900

    feat: Create Order_detail
    - create order_detail
    - create reviewlist
    - create reviewcreate

--- shinhantoy/order/admin.py
+++ shinhantoy/order/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Order
+from .models import Order, Comment
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
     pass
--- shinhantoy/order/views.py
+++ shinhantoy/order/views.py
@@ -1,8 +1,12 @@
 from rest_framework import generics, mixins
 from rest_framework.response import Response
-from .models import Order
-from .serializers import OrderSerializer
+from .serializers import (
+    OrderSerializer,
+    CommentSerializer,
+    CommentCreateSerializer,
+)
 from .paginations import OrderLargePagination
+from .models import Order, Comment
 
 
 class OrderListView(
@@ -16,3 +20,44 @@ class OrderListView(
         
     def get(self, request, *args, **kwargs):
         return self.list(request, args, kwargs)
+
+class OrderDetailView(
+    mixins.RetrieveModelMixin,
+    generics.GenericAPIView,
+):
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        return Order.objects.all().order_by('-id')
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, args, kwargs)
+
+class CommentListView(
+    mixins.ListModelMixin,
+    generics.GenericAPIView,
+):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        order_id = self.kwargs.get('order_id')
+        if order_id:
+            return Comment.objects.filter(order_id=order_id) \
+                .select_related('member', 'order') \
+                .order_by('-id')
+        return Comment.objects.none()
+        
+    def get(self, request, *args, **kwargs):
+        return self.list(request, args, kwargs)
+
+class CommentCreateView(
+    mixins.CreateModelMixin,
+    generics.GenericAPIView,
+):
+    serializer_class = CommentCreateSerializer 
+
+    def get_queryset(self):
+        return Comment.objects.all()
+
+    def post(self, request, *args, **kwargs):                    
+        return self.create(request, args, kwargs)
